2c75cff095fc45d21b468e320adcbaecb5e508c2
Betsy-tr <betsytramma@gmail.com>
2023-07-27 10:50:04 -0400

    modification sur Contact

--- src/common/Conact.jsx
+++ src/common/Conact.jsx
@@ -8,6 +8,7 @@ const Conact = () => {
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
                 <form action="#" className="space-y-8">
                     <div>
+                        <h1>HELLO LA TEAM CDA</h1>
                         <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                         <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
                     </div>
