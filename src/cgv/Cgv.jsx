2872334730b90e1657d28dc82b1560bdb9732001
Alyss-Aelle <alyssferjul@gmail.com>
2023-07-27 15:55:36 -0400

    modif cgv

--- src/cgv/Cgv.jsx
+++ src/cgv/Cgv.jsx
@@ -1,17 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const Cgv = () => {
-    return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">CGV</h1>
-                    <p className="py-6"> Premierement cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
-                </div>
-            </div>
+  return (
+    <>
+    <div className="hero  pt-60 bg-base-200 ">
+      <div className="hero-content text-center ">
+        <div className="">
+          <h1 className="text-5xl font-bold pb-40">
+            CONDITIONS GENERALES DU CONCOURS <br />
+            "Une idée, Une App"
+          </h1>
+          <h2 className="text-4xl pt-7 font-bold">Préambule</h2>
+          <p className="py-6 pb-20">
+            Premierement cupiditate voluptatem et in. Quaerat fugiat ut
+            assumenda excepturi exercitationem quasi. In deleniti eaque aut
+            repudiandae et a id nisi. Premierement cupiditate voluptatem et in.
+            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+            deleniti eaque aut repudiandae et a id nisi. Premierement cupiditate
+            voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id
+            nisi.
+          </p>
         </div>
-    )
-}
+        </div>
+        </div>
 
-export default Cgv
+    <div className="Article  bg-base-200">
+        <div className=" mt-12 py-6 p-40">
+            <h1 className="font-bold text-2xl pt-5 pb-5 flex justify-center">
+                Article 1 : Inscription
+            </h1>
+            <p>Premierement cupiditate voluptatem et in. Quaerat fugiat ut
+            assumenda excepturi exercitationem quasi. In deleniti eaque aut
+            repudiandae et a id nisi. Premierement cupiditate voluptatem et in.
+            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+            deleniti eaque aut repudiandae et a id nisi. Premierement cupiditate
+            voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id
+            nisi.</p>
+       
+
+    </div>
+
+
+    <div className=" py-6 p-40">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center">
+                Article 2 : Concours 
+            </h1>
+            <p>Premierement cupiditate voluptatem et in. Quaerat fugiat ut
+            assumenda excepturi exercitationem quasi. In deleniti eaque aut
+            repudiandae et a id nisi. Premierement cupiditate voluptatem et in.
+            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+            deleniti eaque aut repudiandae et a id nisi. Premierement cupiditate
+            voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id
+            nisi.</p>
+       
+
+    </div>
+        </div>
+    </>
+  );
+};
+
+export default Cgv;
