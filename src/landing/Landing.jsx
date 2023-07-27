3e131dd4b7182b5d665af71ad2466bf1a9f5dde2
steeven-js <jacques.steeven@gmail.com>
2023-07-27 11:41:46 -0400

    wip

--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -1,28 +1,17 @@
-import React from 'react'
-import Hero from '../common/Hero'
-import Cta from '../common/Cta'
-import Content from '../common/Content'
-import Features from '../common/Features'
-import Team from '../common/Team'
-import Footer from '../common/Footer'
-import Gallery from '../common/Gallery'
-import Contact from '../common/Contact'
-import Navbar from '../common/Navbar'
+import '../App.css'
 
-const Landing = () => {
+import Navbar from '../common/Navbar'
+import Hero from '../common/Hero'
+import Footer from '../common/Footer'
+
+function App() {
   return (
     <>
-      <Navbar />
-      {/*<Hero />*/}
-      {/* <Cta /> */}
-      <Content />
-      {/*<Features />
-      <Gallery />
-      <Team />*/}
-      <Contact />
-      <Footer />
+      <Navbar/>
+      <Hero/>
+      <Footer/>
     </>
   )
 }
 
-export default Landing
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-
-import colors from 'tailwindcss/colors'
-
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
   ],
-  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
-      }
-    },
-    fontFamily: {
-      'body': [
-        'Inter',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'system-ui',
-        'Segoe UI',
-        'Roboto',
-        'Helvetica Neue',
-        'Arial',
-        'Noto Sans',
-        'sans-serif',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        'Noto Color Emoji'
-      ],
-      'sans': [
-        'Inter',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'system-ui',
-        'Segoe UI',
-        'Roboto',
-        'Helvetica Neue',
-        'Arial',
-        'Noto Sans',
-        'sans-serif',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        'Noto Color Emoji'
-      ]
-    }
+    extend: {},
   },
-  plugins: [
-    require("daisyui"),
-    require('flowbite/plugin'),
-  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-text-case": "uppercase", // set default text transform for buttons
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+      "dark",
+      "retro",
+    ],
+  },
+plugins: [
+  require("daisyui"),
+],
 }
 
