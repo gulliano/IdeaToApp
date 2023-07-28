5d19da75b6b50c47023ac311c5a9cbbc640ff382
gulliano <ggulliano@msn.com>
2023-07-28 10:37:15 -0400

    add store to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import { Routes, Route,  Link } from "react-router-dom";
 import Landing from "./landing/Landing";
 import Cgv from "./cgv/Cgv";
 import Layout from "./layout/Layout";
+import { Provider } from 'react-redux';
+
+import { store } from './redux';
+import Inscription from "./inscription/Inscription";
 
 
 export default function App() {
   return (
-    <div>
-       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path="cgv" element={<Cgv />} />
-        
-
-     
-          <Route path="*" element={<Landing />} />
-        </Route>
-      </Routes>
-    </div>
+    <Provider store={store} >
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Landing />} />
+            <Route path="cgv" element={<Cgv />} />
+            <Route path="inscription" element={<Inscription />} />        
+            <Route path="*" element={<Landing />} />
+          </Route>
+        </Routes>
+      </div>
+    </Provider>
   );
 }
