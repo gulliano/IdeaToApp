96ed95140bb833145f55a309ee551d38534e8e84
Alyss-Aelle <alyssferjul@gmail.com>
2023-07-28 12:27:52 -0400

    ajout contenu cgv

--- src/cgv/Cgv.jsx
+++ src/cgv/Cgv.jsx
@@ -23,38 +23,108 @@ const Cgv = () => {
         </div>
 
     <div className="Article  ">
-        <div className=" mt-12 py-6 p-40">
-            <h1 className="font-bold text-2xl pt-5 pb-5 flex justify-center ">
-                Article 1 : Inscription
+        <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pt-5 pb-5 flex justify-center text-center">
+                Article 1 : Organisateur 
             </h1>
-            <p>Premierement cupiditate voluptatem et in. Quaerat fugiat ut
-            assumenda excepturi exercitationem quasi. In deleniti eaque aut
-            repudiandae et a id nisi. Premierement cupiditate voluptatem et in.
-            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-            deleniti eaque aut repudiandae et a id nisi. Premierement cupiditate
-            voluptatem et in. Quaerat fugiat ut assumenda excepturi
-            exercitationem quasi. In deleniti eaque aut repudiandae et a id
-            nisi.</p>
+            <p>La team CDA est un groupe émergent de Concepteurs Développeurs d'Applications passionnés par le monde du web. Unis par notre volonté commune d’avancer dans notre domaine, nous travaillons ensemble à approfondir et affiner nos connaissances et compétences.
+              <br />
+
+Ensemble, nous explorons les technologiques qui façonne le web afin de repousser sans cesses nos limites dans l'innovation. Notre objectif est de créer des applications web et mobile de qualité, tout en restant à l'affût des évolutions de notre industrie.
+</p>
        
 
     </div>
 
 
-    <div className=" py-6 p-40">
-            <h1 className="font-bold text-2xl pb-5 flex justify-center ">
-                Article 2 : Concours 
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 2 : Éligibilité 
             </h1>
-            <p>Premierement cupiditate voluptatem et in. Quaerat fugiat ut
-            assumenda excepturi exercitationem quasi. In deleniti eaque aut
-            repudiandae et a id nisi. Premierement cupiditate voluptatem et in.
-            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-            deleniti eaque aut repudiandae et a id nisi. Premierement cupiditate
-            voluptatem et in. Quaerat fugiat ut assumenda excepturi
-            exercitationem quasi. In deleniti eaque aut repudiandae et a id
-            nisi.</p>
+            <p>Toutes personnes souhaitant candidater doit être impérativement majeur. <br />
+Ce concours s’adresse a un public professionnel ou non, avec une idée d'application web ou mobile qu'ils souhaitent soumettre au concours. Les candidats doivent être en mesure de fournir tous les documents et informations requis pour leur candidature et être disponible pour les sessions de travail. 
+</p>
        
 
     </div>
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 3 : Candidature 
+            </h1>
+            <p>Les candidats devront répondre à un formulaire en ligne où ils détailleront leurs motivations, leur parcours et leur projet. Il sera également possible de joindre des documents supplémentaires au format PDF ou JPG, tels que des schémas, des croquis ou autres éléments, pour enrichir leur candidature. 
+</p>
+       
+
+    </div>
+
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 4 : Processus de sélection  
+            </h1>
+            <p>Toutes les candidatures reçues seront examinés. 
+Le jury, composé des membres de la team CDA, sélectionnera un certain nombre de projets en fonction du profil du porteur de projet et de la faisabilité du projet. 
+</p>
+
+    </div>
+
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 5 : Réalisation des projets sélectionnés   
+            </h1>
+            <p>Les projets sélectionnés seront réalisés gratuitement sous format MVP (minimum valuable product). Aucun partenariat commercial ne sera établi entre les porteurs de projet et la team CDA. avec un suivi régulier pour montrer notre avancée.
+</p>
+
+    </div>
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 6 : Droits à l'image   
+            </h1>
+            <p>Les candidats aux différentes phases du concours acceptent par avance d’être photographiés et que le texte de leur discours, leur enregistrement sonore ou vidéo, ainsi que leur image puissent faire l’objet d’une communication par team CDA.
+</p>
+
+    </div>
+
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 7 : Acceptation du règlement   
+            </h1>
+            <p>La participation au concours implique automatiquement l'acceptation pure et simple du présent règlement. Toute tentative de fraude ou de non-respect des conditions générales entraînera la disqualification du candidat concerné.
+</p>
+
+    </div>
+
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 8 : Responsabilités   
+            </h1>
+            <p>La team CDA se réserve le droit de modifier ou d'annuler le concours en cas de circonstances imprévues ou de force majeure. Elle ne pourra en aucun cas être tenue responsable des dommages directs ou indirects causés par la participation au concours ou par l'utilisation des projets réalisés.
+
+</p>
+
+    </div>
+
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 9 : Confidentialité   
+            </h1>
+            <p>La team CDA s'engage à préserver la confidentialité des idées soumises par les candidats et à ne pas divulguer d'informations sensibles sans le consentement explicite du porteur de projet. Hors communication sur l’avancer des projets sélectionnés.
+
+
+</p>
+
+    </div>
+
+    <div className=" mt-12 py-6 p-40 max-md:p-10">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center text-center">
+                Article 10 : Contact   
+            </h1>
+            <p>Pour toute question ou demande d'information concernant le concours, les candidats peuvent contacter la team CDA via le formulaire contact sur la page web du concours.
+
+
+</p>
+
+    </div>
+
         </div>
     </>
   );
