b10a138fc4dc5ee3d60c7e8ab39c30af3d0f5800
steeven-js <jacques.steeven@gmail.com>
2023-07-28 08:16:37 -0400

    fond blanc

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,26 +11,24 @@ export default {
     themes: [
       {
         mytheme: {
-          "primary": "#d6ba1b",
-          "secondary": "#7a75d8",
-          "accent": "#e876cf",
-          "neutral": "#242a2e",
-          "base-100": "#4b3456",
-          "info": "#667ff0",
-          "success": "#2dd793",
-          "warning": "#977111",
-          "error": "#e56675",
 
-          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
-          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
-          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
-          "--animation-btn": "0.25s", // duration of animation when you click on button
-          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
-          "--btn-text-case": "uppercase", // set default text transform for buttons
-          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
-          "--border-btn": "1px", // border width of buttons
-          "--tab-border": "1px", // border width of tabs
-          "--tab-radius": "0.5rem", // border radius of tabs
+          "primary": "#fbbd23",
+
+          "secondary": "#f000b8",
+
+          "accent": "#1dcdbc",
+
+          "neutral": "#2b3440",
+
+          "base-100": "#ffffff",
+
+          "info": "#3abff8",
+
+          "success": "#36d399",
+
+          "warning": "#fbbd23",
+
+          "error": "#f87272",
         },
       },
       "dark",
