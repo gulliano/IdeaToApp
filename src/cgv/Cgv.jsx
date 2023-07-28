6529324b6bd4eb06e5fbcc6eba4f0912632bc1ab
Alyss-Aelle <alyssferjul@gmail.com>
2023-07-28 09:31:25 -0400

    modif couleur cgv

--- src/cgv/Cgv.jsx
+++ src/cgv/Cgv.jsx
@@ -3,31 +3,28 @@ import React from "react";
 const Cgv = () => {
   return (
     <>
-    <div className="hero  pt-60 bg-base-200 ">
+    <div className="hero  pt-60 bg-white ">
       <div className="hero-content text-center ">
         <div className="">
-          <h1 className="text-5xl font-bold pb-40">
-            CONDITIONS GENERALES DU CONCOURS <br />
-            "Une idée, Une App"
+          <h1 className="text-5xl font-bold pb-5 text-pink-600">
+            CONDITIONS GENERALES DU CONCOURS 
+            
           </h1>
-          <h2 className="text-4xl pt-7 font-bold">Préambule</h2>
+          <h2 className="text-5xl font-bold pb-20 text-amber-400 ">"Une idée, Une App"</h2>
+          <h2 className="text-4xl pt-7 font-bold ">Préambule</h2>
           <p className="py-6 pb-20">
-            Premierement cupiditate voluptatem et in. Quaerat fugiat ut
-            assumenda excepturi exercitationem quasi. In deleniti eaque aut
-            repudiandae et a id nisi. Premierement cupiditate voluptatem et in.
-            Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-            deleniti eaque aut repudiandae et a id nisi. Premierement cupiditate
-            voluptatem et in. Quaerat fugiat ut assumenda excepturi
-            exercitationem quasi. In deleniti eaque aut repudiandae et a id
-            nisi.
-          </p>
+          "une idée une application" est un concours organisé par la team CDA 2023 stagiaires à l'IMFPA en Martinique.
+            "une idée une application" à pour but de montrer nos compétences de concepteur dans le monde du développement et de faire émmerger des idées d'applications.
+            Il s'agit d'un concours qui nous permettra ; nous team cda ; à l'issue d'un vote , de choisir un projet et de le concevoir GRATUITEMENT.
+            Ce concours est avant tout un moyen d'expression non officiel et s'inscrit dans une démarche bienveillante et créative.
+                    </p>
         </div>
         </div>
         </div>
 
-    <div className="Article  bg-base-200">
+    <div className="Article  ">
         <div className=" mt-12 py-6 p-40">
-            <h1 className="font-bold text-2xl pt-5 pb-5 flex justify-center">
+            <h1 className="font-bold text-2xl pt-5 pb-5 flex justify-center ">
                 Article 1 : Inscription
             </h1>
             <p>Premierement cupiditate voluptatem et in. Quaerat fugiat ut
@@ -44,7 +41,7 @@ const Cgv = () => {
 
 
     <div className=" py-6 p-40">
-            <h1 className="font-bold text-2xl pb-5 flex justify-center">
+            <h1 className="font-bold text-2xl pb-5 flex justify-center ">
                 Article 2 : Concours 
             </h1>
             <p>Premierement cupiditate voluptatem et in. Quaerat fugiat ut
