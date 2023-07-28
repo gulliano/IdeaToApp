718f6252212b4790b713b23f92d15756f9eb1c93
Betsy-tr <betsytramma@gmail.com>
2023-07-28 09:17:59 -0400

    enregistrement en base

--- src/common/Contact.jsx
+++ src/common/Contact.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import handleSubmit from '../commonjs/handlesubmit'; 
-import { useRef } from 'react';
+import { useRef , useState } from 'react';
+
+
 
 const Conact = () => {
+
+    const [errorNom, setErrorNom] = useState(false);
+    const [errorPrenom, setErrorPrenom] = useState(false);
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
 
     const dataRef = useRef()
  
     const submithandler = (e) => {
         e.preventDefault()
-      const  { nom ,  prenom , email , message} = e.target ;
-      console.log(' nom ,  prenom , email , message' ,  nom.value ,  prenom.value , email.value , message.value) ; 
+        const  { nom ,  prenom , email , message} = e.target ;
+        console.log(' nom ,  prenom , email , message' ,  nom.value ,  prenom.value , email.value , message.value) ; 
+    
+
+       // Vérification des champs, si les champs sont vide on affiche le error
+      (nom.value == "")?setErrorNom(true):setErrorNom(false) ;
+      (prenom.value == "")?setErrorPrenom(true):setErrorPrenom(false) ;
+      (email.value == "")?setErrorEmail(true):setErrorEmail(false) ;
+      (message.value == "")?setErrorMessage(true):setErrorMessage(false) ;
+
+      if(!(errorNom && errorPrenom && errorEmail && errorMessage)){
         handleSubmit(nom.value ,  prenom.value , email.value , message.value)
-       dataRef.current.value = ""
+        dataRef.current.value = ""
+      }
+
+    //     handleSubmit(nom.value ,  prenom.value , email.value , message.value)
+    //    dataRef.current.value = ""
     }
     
     return (
@@ -26,19 +46,31 @@ const Conact = () => {
                         <div className="card flex-shrink-0 w-full max-w-lg min-h-fit shadow-2xl bg-base-100">
                             <div className="card-body">
                                 <div className="form-control">
-                                    <input type="text" ref={dataRef} name="nom" placeholder="Nom" className="input input-bordered" />  
+                                    <input type="text" ref={dataRef} name="nom" placeholder="Nom" className="input input-bordered" />
+                                    {errorNom&&<label className="label">
+                                        <span className="label-text-alt text-pink-600">Vous devez saisir votre nom !</span>
+                                    </label>}
                                 </div>
 
                                 <div className="form-control">
-                                    <input type="text" ref={dataRef} name="prenom" placeholder="Prenom" className="input input-bordered" />  
+                                    <input type="text" ref={dataRef} name="prenom" placeholder="Prenom" className="input input-bordered" />
+                                    {errorPrenom&&<label className="label">
+                                        <span className="label-text-alt text-pink-600">Vous devez saisir votre prenom !</span>
+                                    </label>}  
                                 </div>
 
                                 <div className="form-control">
-                                    <input type="text" ref={dataRef} name="email" placeholder="Email" className="input input-bordered" />  
+                                    <input type="text" ref={dataRef} name="email" placeholder="Email" className="input input-bordered" />
+                                    {errorEmail&&<label className="label">
+                                        <span className="label-text-alt text-pink-600">Votre email est obligatoire !</span>
+                                    </label>}  
                                 </div>
         
                                 <div className="form-control">
                                     <textarea type="text" ref={dataRef} name="message" placeholder="Saisie ton message" className="textarea textarea-bordered w-full resize-none rows-15 cols-70"/>
+                                    {errorMessage&&<label className="label">
+                                        <span className="label-text-alt text-pink-600">Merci de saisir votre message !</span>
+                                    </label>}
                                 </div>
                                 <div className="form-control mt-6">
                                     <button type = "submit" className="btn btn-primary">Envoyer</button>
