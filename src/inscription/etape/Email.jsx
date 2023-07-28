ae136599a4a23fd58e93d19e004140769e733102
gulliano <ggulliano@msn.com>
2023-07-28 11:04:35 -0400

    email

--- src/inscription/etape/Email.jsx
+++ src/inscription/etape/Email.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { useDispatch } from 'react-redux'
-import { plus, moins , reset } from '../../redux';
+import { plus,  addInscription } from '../../redux';
 
 const Email = () => {
   const dispatch = useDispatch() ;
+
+  const [email, setEmail] = useState('gustave')
+  const [prenom, setTelephone] = useState('gulliano')
+
+  const [errEmail, setErrEmail] = useState(false)
+  const [errTelephone, setErrTelephone] = useState(false)
+
+  const handleSubmit = (event) => { 
+    event.preventDefault() ;
+    
+ const { email , telephone} =  event.target ;
+  console.log('prenom :  ' ,event.target  ) ;
+
+
+   const info = { email : email, 
+                  telephone : telephone,
+                            
+      }
+
+   
+        console.log("infoStagiaire" , info) 
+      dispatch(addInscription(info)) ; 
+      dispatch(plus())
+
+   }
+
+   const changeEmail = (event) => {
+    setEmail(event.target.value);
+    event.target.value =='' ? setErrEmail(true):setErrEmail(false) ;
+  }
+  const changeTelephone = (event) => {
+    setTelephone(event.target.value) ;
+    event.target.value =='' ? setErrTelephone(true):setErrTelephone(false) ;
+  }
+
   return (
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -17,19 +52,23 @@ const Email = () => {
           <label className="label">
             <span className="label-text">Ton adresse Email</span>
           </label>
-          <input type="text" placeholder="Email" className="input input-bordered" />
-        </div>
+          <input type="text" value={email} onChange={event=>changeEmail(event)} placeholder="Email" className="input input-bordered" />
+        
+          <label className="label hidden">
+           Une adresse email valide est obligatoire 
+          </label>
+          </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Ton téléphone</span>
           </label>
-          <input type="text" placeholder="06XXXXXXX" className="input input-bordered" />
+          <input type="text" value={telephone} onChange={event=>changeTelephone(event)} placeholder="06XXXXXXX" className="input input-bordered" />
           <label className="label hidden">
-            Forgot password?
+            Tu dois saisir ton téléphone
           </label>
         </div>
         <div className="form-control mt-6">
-          <button onClick={()=>dispatch(plus()) } className="btn btn-primary">Login</button>
+          <button onClick={()=>dispatch(plus()) } className="btn btn-primary">Etape suivante</button>
         </div>
       </div>
     </div>
--- src/inscription/etape/Presentation.jsx
+++ src/inscription/etape/Presentation.jsx
@@ -9,6 +9,9 @@ const Presentation = () => {
   const [nom, setNom] = useState('gustave')
   const [prenom, setPrenom] = useState('gulliano')
 
+  const [errNom, setErrNom] = useState(false)
+  const [errPrenom, setErrPrenom] = useState(false)
+
   const handleSubmit = (event) => { 
     event.preventDefault() ;
     
@@ -21,14 +24,21 @@ const Presentation = () => {
                             
       }
 
-    const changeNom = (event) => {
-      setNom(event.target.value)
-    }
+   
         console.log("infoStagiaire" , info) 
       dispatch(addInscription(info)) ; 
-   // dispatch(plus())
+      dispatch(plus())
 
    }
+
+   const changeNom = (event) => {
+    setNom(event.target.value);
+    event.target.value =='' ? setErrNom(true):setErrNom(false) ;
+  }
+  const changePrenom = (event) => {
+    setPrenom(event.target.value) ;
+    event.target.value =='' ? setErrPrenom(true):setErrPrenom(false) ;
+  }
 
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -45,15 +55,19 @@ const Presentation = () => {
             <span className="label-text">Ton nom</span>
           </label>
           <input type="text"  onChange={event=>changeNom(event)} value={nom}  name='prenom' placeholder="Nom"   className="input input-bordered" />
+          { errNom &&  <label className="label ">
+               Tu dois saisir ton nom
+          </label>}
+         
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Ton prénom</span>
           </label>
-          <input type="text"   onChange={event=>setPrenom(event.target.value)} value={prenom}  name='prenom' placeholder="Prénom" className="input input-bordered" />
-          <label className="label hidden">
-               Tu dois saissir ton prénom 
-          </label>
+          <input type="text"   onChange={event=>changePrenom(event)} value={prenom}  name='prenom' placeholder="Prénom" className="input input-bordered" />
+         { errPrenom && <label className="label ">
+               Tu dois saisir ton prénom 
+          </label>}
         </div>
         <div className="form-control mt-6">
           <button type='submit' className="btn btn-primary">Etape suivante </button>
