37ac3226cc07e5cd6f5d774eafa1222548b27571
gulliano <ggulliano@msn.com>
2023-07-31 16:06:41 -0400

    team

--- src/landing/Team.jsx
+++ src/landing/Team.jsx
@@ -12,13 +12,13 @@ const Team = () => {
                     <div className="carousel-item w-1/2">
                         <div className="items-center rounded-lg sm:flex">
                             <a href="#">
-                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.licdn.com/dms/image/D4E35AQHngz1c2SwL4A/profile-framedphoto-shrink_200_200/0/1690820227257?e=1691427600&v=beta&t=v4WXAsLChoDbZsyc6gmczCtGIxWZy5McjvuzZaPc6TA" alt="Anne NATCHIMIE" />
+                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.licdn.com/dms/image/C4E03AQGqiu8b5BVA2w/profile-displayphoto-shrink_200_200/0/1579024986438?e=1696464000&v=beta&t=y24r0v3A1Frd_BnIU69p9kwjP4KdyjvLMxSN4YHFIMM" alt="Didier VERONIQUE" />
                             </a>
                             <div className="p-5">
                                 <h3 className="text-xl font-bold tracking-tight text-gray-900">
-                                    <a href="#">Anne NATCHIMIE</a>
+                                    <a href="#">Didier VERONIQUE</a>
                                 </h3>
-                                <p className="mt-3 mb-4 font-light text-gray-500">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                                <p className="mt-3 mb-4 font-light text-gray-500">la conception ma passion.</p>
                                 <ul className="flex space-x-4 sm:mt-0">
                                     <li>
                                         <a href="https://www.linkedin.com/in/anne-natchimie-865221269/" target='_blank' className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -33,13 +33,34 @@ const Team = () => {
                     <div className="carousel-item w-1/2">
                         <div className="items-center rounded-lg sm:flex">
                             <a href="#">
-                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.licdn.com/dms/image/D4E35AQEYOYedEgx97Q/profile-framedphoto-shrink_200_200/0/1689857133773?e=1691427600&v=beta&t=bTNNHl_TNMfHhAQcViTkx2RLwXEgWIaA60Kdd02Lt8U" alt="Ludmilla HARNAIS" />
+                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.licdn.com/dms/image/D4E35AQHngz1c2SwL4A/profile-framedphoto-shrink_200_200/0/1690820227257?e=1691427600&v=beta&t=v4WXAsLChoDbZsyc6gmczCtGIxWZy5McjvuzZaPc6TA" alt="Anne NATCHIMIE" />
+                            </a>
+                            <div className="p-5">
+                                <h3 className="text-xl font-bold tracking-tight text-gray-900">
+                                    <a href="#">Anne NATCHIMIE</a>
+                                </h3>
+                                <p className="mt-3 mb-4 font-light text-gray-500">J'aime le développement web car il permet d'accroître ma créativité.</p>
+                                <ul className="flex space-x-4 sm:mt-0">
+                                    <li>
+                                        <a href="https://www.linkedin.com/in/anne-natchimie-865221269/" target='_blank' className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item w-1/2">
+                        <div className="items-center rounded-lg sm:flex">
+                            <a href="#">
+                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.licdn.com/dms/image/D4E03AQHhho9CCb1MXQ/profile-displayphoto-shrink_200_200/0/1690828225005?e=1696464000&v=beta&t=uzTdQ870vGZEQsbIRWMH-sdAp--hXEK0MI0f2TH-2ao" alt="Ludmilla HARNAIS" />
                             </a>
                             <div className="p-5">
                                 <h3 className="text-xl font-bold tracking-tight text-gray-900">
                                     <a href="#">Ludmilla HARNAIS</a>
                                 </h3>
-                                <p className="mt-3 mb-4 font-light text-gray-500">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                                <p className="mt-3 mb-4 font-light text-gray-500">La conception et le développement m'aide a développer ma curiosité et mon esprit d'équipe.</p>
                                 <ul className="flex space-x-4 sm:mt-0">
                                     <li>
                                         <a href="https://www.linkedin.com/in/ludmilla-harnais-8774b4235/" target='_blank' className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -60,7 +81,7 @@ const Team = () => {
                                 <h3 className="text-xl font-bold tracking-tight text-gray-900">
                                     <a href="#">Steeven JACQUES</a>
                                 </h3>
-                                <p className="mt-3 mb-4 font-light text-gray-500">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                                <p className="mt-3 mb-4 font-light text-gray-500">Avec notre application web, nous cherchons à répondre à un besoin spécfique du marché.</p>
                                 <ul className="flex space-x-4 sm:mt-0">
                                     <li>
                                         <a href="https://www.linkedin.com/in/steeven-jacques-1337a9238/" target='_blank' className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
