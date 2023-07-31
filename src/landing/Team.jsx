0039e5252ad6ecd70d42d0d136fec9352f88cf26
gulliano <ggulliano@msn.com>
2023-07-31 09:27:46 -0400

    background contact

--- src/landing/Team.jsx
+++ src/landing/Team.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Team = () => {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                 <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
-                    <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classNamees from Tailwind</p>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Our Team</h2>
+                    <p className="font-light text-gray-500 lg:mb-16 sm:text-xl">Explore the whole collection of open-source web components and elements built with the utility classNamees from Tailwind</p>
                 </div>
                 <div className="grid grid-cols-2 gap-8 mb-6 lg:mb-16 md:grid-cols-2">
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex">
                         <a href="#">
                             <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Avatar" />
                         </a>
                         <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            <h3 className="text-xl font-bold tracking-tight text-gray-900">
                                 <a href="#">Bonnie Green</a>
                             </h3>
-                            <span className="text-gray-500 dark:text-gray-400">CEO & Web Developer</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                            <span className="text-gray-500">CEO & Web Developer</span>
+                            <p className="mt-3 mb-4 font-light text-gray-500">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
                             <ul className="flex space-x-4 sm:mt-0">
                                 <li>
                                     <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -43,16 +43,16 @@ const Team = () => {
                             </ul>
                         </div>
                     </div>
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex">
                         <a href="#">
                             <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar" />
                         </a>
                         <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            <h3 className="text-xl font-bold tracking-tight text-gray-900">
                                 <a href="#">Jese Leos</a>
                             </h3>
-                            <span className="text-gray-500 dark:text-gray-400">CTO</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Jese drives the technical strategy of the flowbite platform and brand.</p>
+                            <span className="text-gray-500">CTO</span>
+                            <p className="mt-3 mb-4 font-light text-gray-500">Jese drives the technical strategy of the flowbite platform and brand.</p>
                             <ul className="flex space-x-4 sm:mt-0">
                                 <li>
                                     <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -77,16 +77,16 @@ const Team = () => {
                             </ul>
                         </div>
                     </div>
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex">
                         <a href="#">
                             <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="Michael Avatar" />
                         </a>
                         <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            <h3 className="text-xl font-bold tracking-tight text-gray-900">
                                 <a href="#">Michael Gough</a>
                             </h3>
-                            <span className="text-gray-500 dark:text-gray-400">Senior Front-end Developer</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Michael drives the technical strategy of the flowbite platform and brand.</p>
+                            <span className="text-gray-500">Senior Front-end Developer</span>
+                            <p className="mt-3 mb-4 font-light text-gray-500">Michael drives the technical strategy of the flowbite platform and brand.</p>
                             <ul className="flex space-x-4 sm:mt-0">
                                 <li>
                                     <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -111,16 +111,16 @@ const Team = () => {
                             </ul>
                         </div>
                     </div>
-                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                    <div className="items-center bg-gray-50 rounded-lg shadow sm:flex">
                         <a href="#">
                             <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" alt="Sofia Avatar" />
                         </a>
                         <div className="p-5">
-                            <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            <h3 className="text-xl font-bold tracking-tight text-gray-900">
                                 <a href="#">Lana Byrd</a>
                             </h3>
-                            <span className="text-gray-500 dark:text-gray-400">Marketing & Sale</span>
-                            <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Lana drives the technical strategy of the flowbite platform and brand.</p>
+                            <span className="text-gray-500">Marketing & Sale</span>
+                            <p className="mt-3 mb-4 font-light text-gray-500">Lana drives the technical strategy of the flowbite platform and brand.</p>
                             <ul className="flex space-x-4 sm:mt-0">
                                 <li>
                                     <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
